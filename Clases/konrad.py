ed0485f9efa9237d7cb56f107160acd0683de63b
malondonoc <malondonoc@MacBook-Air-de-Manuel-2.local>
2014-07-05 10:23:16 -0500

    Clases Python

---
+++ Clases/konrad.py
@@ -0,0 +1,12 @@
+"""Este el modulo del curso"""
+nombre= "Konrad Lorenz Fundacion Universitaria"
+estudiantes= 25
+URL="www.konradlorenz.edu.co"
+
+'''
+Este modulo se debe importar como import konrad
+OJO: No se usa la extensión del archivo
+Para consultar que tiene el módulo se puede utilizar dir(konrad)
+La "documentación" se accede como konrad.__doc__
+También se puede usar from konrad import * para no utilizar la notación . 
+'''
